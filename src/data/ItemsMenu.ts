8a9055045b594292fd0e2e0be265ffc05f79e5be
Weirdmeats <165041244+Weirdmeats@users.noreply.github.com>
2025-11-29 03:07:46 -0300

    Actualizacion de footer, drycode
    astro-frontend
    29/11/2025
    +footer
    +drycode title, nav, nvfooter etc

---
+++ src/data/ItemsMenu.ts
@@ -0,0 +1,53 @@
+//navbar principal
+interface NavbarItems {
+    href: string,
+    texto: string,
+    link?: string
+}
+
+export const NavbarMain: NavbarItems[] =[
+    {href:"/", texto: "casting"},
+    {href:"/", texto: "vodcast"},
+    {href:"/", texto: "programas"},
+    {href:"/", texto: "teleseries"},
+    {href:"/", texto: "ntv"},
+    {href:"/", texto: "tv chile"},
+    {href:"/", texto: "noticias"},
+    {href:"/", texto: "señales"},
+]
+
+//navbar secundario
+export const NavbarSecc: NavbarItems[] =[
+    {href:"/", texto: "corporativo y transparencia"},
+    {href:"/", texto: "canal de denuncias"},
+    {href:"/", texto: "areas de proyectos"},
+  
+]
+
+  
+//menu redes sociales
+export const ItemRedes: NavbarItems[] = [
+    {
+        href: "https://images.vexels.com/media/users/3/223136/isolated/preview/984f500cf9de4519b02b354346eb72e0-icono-de-facebook-redes-sociales.png", 
+        texto: "Facebook", 
+        link: "https://www.facebook.com/tvn.cl/?locale=es_LA"
+    },
+    {
+        href: "https://cdn-icons-png.flaticon.com/512/3983/3983858.png", 
+        texto: "Instagram", 
+        link: "https://www.instagram.com/tvchile_tvn/"
+    },
+    {
+        href: "https://cdn-icons-png.flaticon.com/256/3536/3536445.png", 
+        texto: "Whatsapp", 
+        link: "https://web.whatsapp.com/"
+    }
+]
+
+export const NavFooter: NavbarItems[]=[
+    {href:"/", texto: "comercial"},
+    {href:"/", texto: "concursos"},
+    {href:"/", texto: "tv tiempo"},
+    {href:"/", texto: "politicas de privacidad"},
+    {href:"/", texto: "trabaja con nosotros"},
+]
