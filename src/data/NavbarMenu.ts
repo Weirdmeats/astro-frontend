69dddc5abb9dc246d62e9c5237a3e4bbcc5fb4f1
Weirdmeats <165041244+Weirdmeats@users.noreply.github.com>
2025-11-29 00:28:31 -0300

    Avance de header
    astro-frontend
    29-11-2025

---
+++ src/data/NavbarMenu.ts
@@ -0,0 +1,29 @@
+//navbar principal
+interface NavbarMainItems {
+    href: string,
+    texto: string
+}
+
+export const NavbarMain: NavbarMainItems[] =[
+    {href:"/", texto: "casting"},
+    {href:"/", texto: "vodcast"},
+    {href:"/", texto: "programas"},
+    {href:"/", texto: "teleseries"},
+    {href:"/", texto: "ntv"},
+    {href:"/", texto: "tv chile"},
+    {href:"/", texto: "noticias"},
+    {href:"/", texto: "señales"},
+]
+
+//navbar secundario
+interface NavbarSeccItems {
+    href: string,
+    texto: string
+}
+
+export const NavbarSecc: NavbarSeccItems[] =[
+    {href:"/", texto: "corporativo y transparencia"},
+    {href:"/", texto: "canal de denuncias"},
+    {href:"/", texto: "areas de proyectos"},
+  
+]
